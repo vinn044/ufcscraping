4abb94808db1075da0d06d70c0cecae527e5e8b2
vin <vincek1800@gmail.com>
2026-05-13 03:58:42 -0500

    test 1

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
-df = pd.read_csv("fight_details.csv")
+df = pd.read_csv("UFC.csv")
 
-print(df.head())
-print(df.columns)
-print(df.shape)
+df["red_won"] = (df["winner"] == df["r_name"]).astype(int)
 
-print(df[["r_name", "b_name", "method"]].head(20))
-print(df.iloc[0])
+X = df[[
+    "r_str_acc",
+    "b_str_acc",
+    "r_td_avg",
+    "b_td_avg"
+]]
+
+y = df["red_won"]
+
+model = LogisticRegression()
+
+model.fit(X, y)
+
+print("Model trained!")
+
+
+def get_fighter_stats(name):
+    fighter = df[df["r_name"] == name]
+
+    return {
+        "str_acc": fighter["r_str_acc"].mean(),
+        "td_avg": fighter["r_td_avg"].mean()
+    }
+
+
+fighter_one = "Islam Makhachev"
+fighter_two = "Charles Oliveira"
+
+fighter_one_stats = get_fighter_stats(fighter_one)
+fighter_two_stats = get_fighter_stats(fighter_two)
+
+fight = [[
+    fighter_one_stats["str_acc"],
+    fighter_two_stats["str_acc"],
+    fighter_one_stats["td_avg"],
+    fighter_two_stats["td_avg"]
+]]
+
+prediction = model.predict(fight)
+
+probability = model.predict_proba(fight)
+
+print(f"{fighter_one}: {probability[0][1] * 100:.2f}%")
+print(f"{fighter_two}: {probability[0][0] * 100:.2f}%")
